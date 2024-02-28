89e2079d20bba6ecce87b3a23bb16b7410fbc83b
Glitch (x86-inf-vm) <none>
2024-02-28 13:06:06 +0000

    fuck i forgot i made the table website. might as well flesh it out

--- table_worker.js
+++ table_worker.js
@@ -1,16 +1,16 @@
-import { lower_bound, upper_bound, code_square } from "./table_common.js";
+import { lower_bound, upper_bound, code_add, code_mul } from "./table_common.js";
 
 onmessage = e =>
 {
   let matrix = e.data;
   for (;;)
   {
-    postMessage(matrix);
-    let n = 0;
     for (let i = lower_bound; i <= upper_bound; i++)
-      for (let j = lower_bound; j <= upper_bound; j++)
-        n += !matrix[i][j];
-    if (!n) break;
-    matrix = code_square(matrix);
+    {
+      postMessage(matrix);
+      for (let y = lower_bound; y <= upper_bound; y++)
+        for (let x = lower_bound; x <= upper_bound; x++)
+          matrix[y][x] = code_add(matrix[y][x], code_mul(matrix[y][i], matrix[i][x]));
+    }
   }
 }
